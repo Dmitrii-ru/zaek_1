9b02cbd902a0fb1a3f5f456af4e83d6c95c449c3
Dmitrii-ru <nochev1@mail.ru>
2025-07-30 14:47:25 +0300

    Обновлен

--- core/settings.py
+++ core/settings.py
@@ -87,5 +87,19 @@ LANGUAGE_CODE = 'RU-ru'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-STATIC_URL = 'static/'
+
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),  # Дополнительные папки со статикой
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
